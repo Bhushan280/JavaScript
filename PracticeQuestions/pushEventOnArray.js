21863d87966a1a74bdd6afb821077932bf0aa838
Bhushan280 <bhushanchouhan25@gmail.com>
2025-04-21 16:27:06 +0530

    Push event on array

---
+++ PracticeQuestions/pushEventOnArray.js
@@ -0,0 +1,24 @@
+const originalPush = Array.prototype.push;
+
+Array.prototype.push = function (...args) { 
+    const result = originalPush.apply(this, args);
+    if (this.onPush) {
+        this.onPush(args);
+    }
+    return result
+}
+
+Array.prototype.setPushCb = function (callback) {
+    if (typeof callback === 'function') {
+        this.onPush = callback; // Store the callback function on the array
+    } else {
+        throw new TypeError('Callback must be a function');
+    }
+};
+// Test
+const arr = [];
+arr.setPushCb((items) => {
+ console.log('Items pushed:', items);
+});
+arr.push(1);
+arr.push(2, 3);
